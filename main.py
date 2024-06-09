14b86f357597476866f206097d979e231f4aa438
Raika RC <137921766+raikarc@users.noreply.github.com>
2024-06-09 09:43:02 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,102 @@
+points = 0
+
+print("Hello World")
+
+print()
+print(" What Would YOU Do? ")
+print("---------------------")
+print("The Online Trivia Course That Teaches you About Cyberbullying")
+print("--------------------------------------------------")
+print()
+print("Question 1:")
+print("An individual is posting mean or embarrasing things about you. How do you react?")
+print(" a) Ignore them! They don't need my attention!")
+print(" b) Be mean back! That'll show them!")
+a1 = input("Your selection (a / b): ")
+if a1 == "a" or a1 == "A":
+  print("That's right! Ignoring them will stop the injustice! Make sure never to ignore your feelings though! Talk to a trusted adult if what the bully was saying hurt you! They'll help you!")
+  points += 1
+elif a1 == "b" or a1 == "B":
+  print("Sorry, that's incorrect! Giving them a reaction is what the bully wants! Ignore them instead, which wouldn't give them the satisfaction they sought after!")
+else:
+  print("iNvAlId iNpuT")
+  print("System was unable to recognize input")
+
+print()
+print("Note:")
+print("Digital Drama isn't always cyberbullying! Digital Drama often starts off small, like with a vague social media post. Unlike cyberbullying, digital drama isn't as focused to be mean to one person. It doesn't be repeated constantly, like cyberbullying often does repeat.")
+print()
+
+print("Question 2:")
+print("What is 'shaming' in digital drama?")
+print(" a) Directly focusing on a person, than constantly posting about their flaws, and other, possibly make-believe, information about the person, making them look bad")
+print(" b) Something that doesn't cause a scene, but it is repetitive and hurtful")
+print(" c) Drama that capitalizes on the public or semipublic nature of social media sites in order to embarrass and shame")
+a2 = input("Your selection (a / b / c): ")
+if a2 == "a" or a2 == "A":
+  print("Sorry, that's incorrect. Shaming is more of drama that relies on the public of social media to shame. Since Digital Drama is also 'less focused' than cyberbullying, this direct bullying would be cyberbullying because of its repitition. But both are harmful, and both deserve to be stopped!")
+elif a2 == "b" or a2 == "B":
+  print("Sorry, that's incorrect. Unlike cyberbullying, shaming isn't repetitive. Shaming is actually not as direct drama that uses the nature of social media and the public to shame and embarrass somebody in an indirect sense.")
+elif a2 == "c" or a2 == "C":
+  print("That's correct! Shaming is not that direct, and relies on the public and social media itself. It isn't repetitive, but still harmful, and a form of digital drama.")
+  points += 2
+else:
+  print("iNvAlId iNpuT")
+  print("System was unable to recognize input")
+print()
+print("Note: There are six types of online stress, but there are four main ones. Remember, digital drama is not just cyberbullying.")
+print("Four main types:")
+print(" 1) Being impersonated or hacked")
+print(" 2) Being outed, shamed, or humiliated")
+print(" 3) Recieving mean, and harrassing personal attacks")
+print(" 4) Breaking and entering into accounts or phones")
+print()
+print("Question 3:")
+print("What can you do if you expirience cyberbullying or are a victim of Digital Drama")
+print(" a) Talk to a trusted adult")
+print(" b) Cut off ties with the bully/drama causer")
+print(" c) Ignore the situation")
+print(" d) Use available internet reportion tools")
+print(" e) All of the above")
+a3 = input("Your selection (a / b / c / d /e): ")
+if a3 == "a" or a3 == "A":
+  print("That's right, but all of those options are valid ways to deal with this situation!")
+  points += 1
+elif a3 == "b" or a3 == "B":
+  print("That's right, but all of those options are valid ways to deal with this situation!")
+  points += 1
+elif a3 == "c" or a3 == "C":
+  print("That's right, but all of those options are valid ways to deal with this situation!")
+  points += 1
+elif a3 == "d" or a3 == "D":
+  print("That's right, but all of those options are valid ways to deal with this situation!")
+  points += 1
+elif a3 == "e" or a3 == "E":
+  print("That's right! All of these reactions are appropriate solutions to be made!")
+  points += 3
+else:
+  print("iNvAliD inPuT")
+  print("System ws unable to recognize input")
+print()
+print()
+print("_________________")
+print("|Total Points: " + str(points) + "|")
+print("_________________")
+if points == 6:
+  print("Great job! You got all of them correct! You're ready to spread this knowledge to others, and together we can make an impact!")
+print()
+print()
+print("Concluding Thoughts:")
+print("1) Digital Drama is not just cyberbullying")
+print("2) Digital Drama is less focused on one person, and often uses the social media to its advantage")
+print("3) Trying and keeping a cool mind, not stressing, and being active will help you deal better with Digital Drama")
+print(" Excersize helps keeping you happy, and will therefore help you deal with situations with a cool mindset, energized and thoughtful.")
+print()
+print("So What Do I consider Digital Drama?")
+print("Though cyberbullying isn't the ONLY digital drama, I consider cyberbullying and less abrupt buyyling approaches such as shaming (part of cyberbullying) as Digital Drama")
+print()
+print("Therefore Digital Drama is not that often for one individual, but is a still hurtful and stress-causing. It deserves to be stopped! How about adding the common phrase 'STOMP OUT CYBERBULLYING' with 'DISSOLVE THAT DRAMA!'?")
+print()
+print("DISSOLVE THAT DRAMA")
+print("#needtoknow")
+print("Project created by Raika Roy Choudhury")
